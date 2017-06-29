da86d55fdc30c1be546282cff5d81876fce40305
xcatliu <xcatliu@gmail.com>
2017-06-29 13:20:12 +0800

    Add Access-Control-Allow-Headers

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,4 +1,5 @@
 export default () => async (ctx, next) => {
   ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   await next();
 }
